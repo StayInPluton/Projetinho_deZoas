70049f846192c85a58498bedcfccb60f864acc9e
Pedro Heleno <111394219+StayInPluton@users.noreply.github.com>
2023-08-19 16:11:21 -0300

    Base das funcionalidades da calculadora basica

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function exibirCalculos() {
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    // Get the value of the input element
+    var inputValue = inputElement.value;
+} 
